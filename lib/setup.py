3710229e26847ecf0c42226599c8fa754ad9be6e
tangentstorm <michal.wallace@gmail.com>
2012-12-28 04:00:04 -0600

    added a setup.py file

---
+++ lib/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='webAppWorkshop',
+    version='',
+    packages=[''],
+    package_dir={'': 'lib'},
+    url='',
+    license='',
+    author='michal',
+    author_email='',
+    description='', requires=['gaetestbed']
+)
